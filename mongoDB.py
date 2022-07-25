14ef9436641ffa7de89bc5aa0bdec455eaee382b
Lio-jai <98979021+Lio-jai@users.noreply.github.com>
2022-07-25 13:09:00 +0530

    this is new

---
+++ mongoDB.py
@@ -0,0 +1,7 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://jai235:1234@devicename.ozstxmu.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+def insd():
+    print("dhsdhs")
