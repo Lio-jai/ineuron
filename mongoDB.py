8b65884ef7976aca7ec317b2d5e674dcdef210b1
Lio-jai <98979021+Lio-jai@users.noreply.github.com>
2022-07-28 13:26:09 +0530

    jk

--- mongoDB.py
+++ mongoDB.py
@@ -1,7 +1,22 @@
 import pymongo
 
-client = pymongo.MongoClient("mongodb+srv://jai235:1234@devicename.ozstxmu.mongodb.net/?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://jai2355:1234@cluster0.efchi54.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
-
-def insd():
-    print("dhsdhs")
+jk={
+  {"name":"jayakumar"},
+ {
+  "brand": "Ford",
+  "model": "Mustang",
+  "years": "1964"
+},{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": "1464"}
+,
+    {"brand": "Ford",
+  "model": "Mustang",
+  "yearss": "1364"}
+}
+database=client["name_database"]
+collection=database["name_of_tabel_or data"]
+collection.insert_one(jk)
