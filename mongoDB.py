6490bf22ae08ed38c072e71cee73e821a0bfb3e1
Lio-jai <98979021+Lio-jai@users.noreply.github.com>
2022-07-28 13:31:07 +0530

    jk

--- mongoDB.py
+++ mongoDB.py
@@ -3,13 +3,12 @@ import pymongo
 client = pymongo.MongoClient("mongodb+srv://jai2355:1234@cluster0.efchi54.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
 jk={
-  {"name":"jayakumar"},
- {
-  "brand": "Ford",
+    {"name":"jayakumar"}
+    ,{"brand": "Ford",
   "model": "Mustang",
-  "years": "1964"
-},{
-  "brand": "Ford",
+  "years": "1964"}
+,
+   {"brand": "Ford",
   "model": "Mustang",
   "year": "1464"}
 ,
@@ -19,4 +18,4 @@ jk={
 }
 database=client["name_database"]
 collection=database["name_of_tabel_or data"]
-collection.insert_one(jk)
+collection.insert_many(jk)
